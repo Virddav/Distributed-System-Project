45ee4e2ae5b94e90c8be28f098c8aee162fad55e
virddav <dbrillaud@outlook.com>
2024-04-11 21:33:12 +0200

    First working version of Chang & Roberts mutual exclusion

--- distributed_chang_roberts.py
+++ distributed_chang_roberts.py
@@ -1,10 +1,10 @@
 import sys
 import socket
-import numpy as np
 import threading
 import time
 import hashlib
 import random
+import select
 
 class Jeton:
     def __init__(self, process_id, nb_process, distributed_ports, display_ports, host, UID):
@@ -22,9 +22,14 @@ class Jeton:
         self.join_election = False
         if process_id == nb_process-1:
             self.next_process_id = 0
-            self.start_time = 0 
         else:
             self.next_process_id = process_id + 1 
+        if process_id == 0:
+            self.start_time = 0 
+            self.starting_election = True
+
+        self.nb_moves = 0
+        self.nb_messages = 0
 
         self.lock = threading.Lock()
         self.create_distributed()
@@ -35,21 +40,28 @@ class Jeton:
         self.process_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
         self.process_socket.bind((self.host, self.distributed_ports[self.process_id]))
         self.process_socket.listen(10)
+        self.process_socket.setblocking(False)
         print("Serveur "+str(self.process_id)+ " started")
         self.start_election()
 
     # Function start_jeton: Basic fork accepting all connexion in thread for time improvement
     def start_election(self):
         try:
-            while not self.quit:
-                new_socket, address = self.process_socket.accept()
-                t = threading.Thread(target=self.handle_connection, args=(new_socket,))
-                t.start()
-                t.join()        
+            while int(self.nb_moves) < int(self.nb_process*10):
+                ready_to_read, _, _ = select.select([self.process_socket], [], [], 0.00001)
+                if ready_to_read:
+                    new_socket, address = self.process_socket.accept()
+                    t = threading.Thread(target=self.handle_connection, args=(new_socket,))
+                    t.start()
         except KeyboardInterrupt:
             print("Server stopped.")
         finally :
             self.process_socket.close()
+            if self.process_id == 0:
+                print("serveur closed with " + str(self.nb_messages)+" messages")
+                print("serveur closed in " + str(time.time()-self.start_time)+"seconds")
+            else:
+                print("check process 0 for stats")
 
     # Function handle_connection: Receive message, 
     # if the message is a jeton send it if nothing to do 
@@ -63,10 +75,30 @@ class Jeton:
                 return
             if data.decode()[0] == "E":
                 election, UID, nb_moves, nb_message = data.decode().split(":")
-                if not self.waiting_jeton:
-                    self.send_jeton(nb_moves, nb_message)
+                if UID == self.UID:
+                    if self.join_election:
+                        self.enter_critical_section(nb_moves, nb_message)
+                    else:
+                        self.send_leadership_message(self.UID,nb_moves,nb_message)
+                elif not self.join_election or UID > self.UID:
+                    self.send_election_message(UID, nb_moves, nb_message)
+                elif UID < self.UID:
+                    self.send_election_message(self.UID, nb_moves, nb_message)
+
+            elif data.decode()[0] == "L":
+                election, UID, nb_moves, nb_message = data.decode().split(":")
+                hash_object = hashlib.sha256()
+                random_number = random.randint(0, 1000)
+                hash_object.update((str(self.process_id)+str(random_number)).encode('utf-8'))
+                new_UID = hash_object.hexdigest()
+
+                if self.UID != UID:
+                    self.UID = new_UID
+                    self.send_leadership_message(UID, nb_moves, nb_message)
                 else:
-                    self.enter_critical_section(nb_moves, nb_message)
+                    self.UID = new_UID
+                    self.send_election_message(self.UID, nb_moves, nb_message)   
+                    self.participant = True     
             else:
                 self.prepare_critical_section(data)
             new_socket.close() # close the socket at the end
@@ -74,17 +106,18 @@ class Jeton:
     # Function prepare_critical_section: Stock the data and wait for the jeton
     #  If it's first received message of the last process send the jeton 
     def prepare_critical_section(self, data):
-        if self.process_id == self.nb_process-1 and self.starting_jeton:
-            self.send_jeton(0,0)
-            self.starting_jeton = False
+        if self.process_id == 0 and self.starting_election:
+            self.send_election_message(self.UID, 0,0)
+            self.starting_election = False
             self.start_time = time.time()
+            self.participant = True
         self.data.append(data)
-        self.waiting_jeton = True
-        print("Waiting for jeton " + str(data))
+        self.join_election = True
+        print("Waiting for election " + str(data))
 
     # Function enter_critical_section: Send latest received message to all display and close critical section
     def enter_critical_section(self, nb_moves, nb_message):
-        print("Jeton received sending message " + str(self.data[0]))
+        print("I am the leader, sending message " + str(self.data[0]))
         for port in self.display_ports:
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 try:
@@ -99,53 +132,41 @@ class Jeton:
     # Function close_critical: Remove latest message, send jeton and increase number of move made
     def close_critical(self, nb_moves, nb_message):
         self.data.pop(0)
-        self.send_jeton(int(nb_moves)+1, nb_message) 
+        self.send_leadership_message(self.UID, int(nb_moves)+1, nb_message) 
         if self.data == []:
-            self.waiting_jeton = False
+            self.join_election = False
+        self.participant = False
 
     # Function send_jeton: Send the jeton to neighbor and check multiple ending condition 
     # nb_message is the number total of jeton transmited, increased by 1 every time 
     # nb_moves is the number total of message sent to display
-    def send_jeton(self, nb_moves, nb_message):
+    def send_election_message(self, UID, nb_moves, nb_message):
         nb_message = int(nb_message)+1 
-        message = "JETON:" + str(nb_moves) +":"+ str(nb_message)
-        # Checking if the game has ended and we are the last process on 
-        if int(nb_moves)  < self.nb_process-1 + (self.nb_process*10):
-            # Checking if the game has ended, if yes we send last jeton and end the process using self.quit
-            if int(nb_moves) >= self.nb_process * 10:
-                print("Serveur ended")
-                if self.process_id == self.nb_process-1:
-                    print("The game lasted : " + str(time.time()-self.start_time)+ " second")
-                    print("Serveur ended with jeton sent equal to " + str(int(nb_message))+ "\nSo a total of " + str(int(nb_message)+ (self.nb_process*10)) + " messages circulated during the game")
-                else:
-                    print("See game duration and number of message transmitted in last process")
-                self.quit = True 
-                message = "JETON:" + str(int(nb_moves)+1) +":"+ str(nb_message)
-                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                    try:
-                        s.connect((self.host, self.distributed_ports[self.next_process_id]))
-                        s.send(message.encode())
-                    except ConnectionRefusedError:
-                        print("Couldn't transmit message : "+ message)
-                    finally:
-                        s.close()
-            else:
-                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                    try:
-                        s.connect((self.host, self.distributed_ports[self.next_process_id]))
-                        s.send(message.encode())
-                    except ConnectionRefusedError:
-                        print("Couldn't transmit message : "+ message)
-                    finally:
-                        s.close()
-        else:
-            if self.process_id == self.nb_process-1:
-                print("The game lasted : " + str(time.time()-self.start_time) + " second")
-                print("Serveur ended with jeton sent equal to " + str(int(nb_message))+ "\nSo a total of " + str(int(nb_message) + int(nb_moves)-self.nb_process+1) + " messages circulated during the game")
-            else:
-                print("Server ended")
-                print("See game duration and number of messages transmitted in last process")
-            self.quit = True
+        self.nb_messages = nb_message
+        self.nb_moves = nb_moves
+        message = "ELECTION:" + str(UID) + ":" + str(nb_moves) +":"+ str(nb_message)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            try:
+                s.connect((self.host, self.distributed_ports[self.next_process_id]))
+                s.send(message.encode())
+            except ConnectionRefusedError:
+                print("Couldn't transmit message : "+ message)
+            finally:
+                s.close()
+    
+    def send_leadership_message(self, UID, nb_moves, nb_message):
+        nb_message = int(nb_message)+1 
+        self.nb_messages = nb_message
+        self.nb_moves = nb_moves
+        message = "LEADER:" + str(UID) + ":" + str(nb_moves) +":"+ str(nb_message)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            try:
+                s.connect((self.host, self.distributed_ports[self.next_process_id]))
+                s.send(message.encode())
+            except ConnectionRefusedError:
+                print("Couldn't transmit message : "+ message)
+            finally:
+                s.close()
 
 def main():
     if len(sys.argv) < 6:
@@ -163,7 +184,7 @@ def main():
     host = "localhost"
     hash_object = hashlib.sha256()
     random_number = random.randint(0, 1000)
-    hash_object.update(str(num_id)+str(random_number)).encode('utf-8')
+    hash_object.update((str(num_id)+str(random_number)).encode('utf-8'))
     UID = hash_object.hexdigest()
 
     print(UID)
